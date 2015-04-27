49c52353ec28379b3d163c99716d4f8dab0396fb
wang zuo <wzuoadjusted@gmail.com>
2015-04-27 18:12:13 +0800

    update example tree json data

--- example/app.js
+++ example/app.js
@@ -7,43 +7,52 @@ var App = React.createClass({
   getInitialState() {
     return {
       tree: {
-        module: 'page',
+        module: 'react-ui-tree',
         children: [{
-          module: 'section',
+          module: 'dist',
           children: [{
-            module: 'container',
-            children: [{
-              module: 'row',
-              children: [{
-                module: 'col',
-                children: []
-              }, {
-                module: 'col',
-                children: []
-              }]
-            }, {
-              module: 'row',
-              children: []
-            }, {
-              module: 'row',
-              children: []
-            }, {
-              module: 'row',
-              children: []
-            }, {
-              module: 'row',
-              children: []
-            }, {
-              module: 'row',
-              children: []
-            }, {
-              module: 'row',
-              children: []
-            }]
+            module: 'node.js'
+          }, {
+            module: 'react-ui-tree.css'
+          }, {
+            module: 'react-ui-tree.js'
+          }, {
+            module: 'tree.js'
           }]
         }, {
-          module: 'section',
-          children: []
+          module: 'example',
+          children: [{
+            module: 'app.js'
+          }, {
+            module: 'app.less'
+          }, {
+            module: 'index.html'
+          }]
+        }, {
+          module: 'lib',
+          children: [{
+            module: 'node.js'
+          }, {
+            module: 'react-ui-tree.js'
+          }, {
+            module: 'react-ui-tree.less'
+          }, {
+            module: 'tree.js'
+          }]
+        }, {
+          module: '.gitiignore'
+        }, {
+          module: 'index.js'
+        }, {
+          module: 'LICENSE'
+        }, {
+          module: 'Makefile'
+        }, {
+          module: 'package.json'
+        }, {
+          module: 'README.md'
+        }, {
+          module: 'webpack.config.js'
         }]
       }
     };
