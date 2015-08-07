c5823fd004fad54950c7054a61abfb481a0305b7
wang zuo <wzuoadjusted@gmail.com>
2015-08-07 17:14:26 +0800

    adding `node.collapsed` support

--- example/app.js
+++ example/app.js
@@ -14,6 +14,7 @@ var App = React.createClass({
         module: 'react-ui-tree',
         children: [{
           module: 'dist',
+          collapsed: true,
           children: [{
             module: 'node.js'
           }, {
@@ -63,10 +64,13 @@ var App = React.createClass({
   },
 
   renderNode(node) {
-    return <span className={cx({
-      'node': true,
-      'is-active': node === this.state.active
-    })} onClick={this.onClickNode.bind(null, node)}>{node.module}</span>;
+    return (
+      <span className={cx('node', {
+        'is-active': node === this.state.active
+        })} onClick={this.onClickNode.bind(null, node)}>
+        {node.module}
+      </span>
+    );
   },
 
   onClickNode(node) {
@@ -83,10 +87,12 @@ var App = React.createClass({
             paddingLeft={20}
             tree={this.state.tree}
             onChange={this.handleChange}
+            isNodeCollapsed={this.isNodeCollapsed}
             renderNode={this.renderNode}
           />
         </div>
         <div className="inspector">
+          <button onClick={this.updateTree}>update tree</button>
           <pre>
           {JSON.stringify(this.state.tree, null, '  ')}
           </pre>
@@ -96,6 +102,14 @@ var App = React.createClass({
   },
 
   handleChange(tree) {
+    this.setState({
+      tree: tree
+    });
+  },
+
+  updateTree() {
+    var tree = this.state.tree;
+    tree.children.push({module: 'test'});
     this.setState({
       tree: tree
     });
