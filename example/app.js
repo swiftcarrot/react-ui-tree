a8847184cbe3d586139662e2f01178662420654a
Wang Zuo <wzuoadjusted@gmail.com>
2015-10-19 18:18:50 +0800

    add tree.js

--- example/app.js
+++ example/app.js
@@ -2,6 +2,8 @@ var cx = require('classnames');
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Tree = require('../lib/react-ui-tree.js');
+var tree = require('./tree');
+
 require('../lib/react-ui-tree.less');
 require('./theme.less');
 require('./app.less');
@@ -10,57 +12,7 @@ var App = React.createClass({
   getInitialState() {
     return {
       active: null,
-
-      tree: {
-        module: 'react-ui-tree',
-        children: [{
-          module: 'dist',
-          collapsed: true,
-          children: [{
-            module: 'node.js'
-          }, {
-            module: 'react-ui-tree.css'
-          }, {
-            module: 'react-ui-tree.js'
-          }, {
-            module: 'tree.js'
-          }]
-        }, {
-          module: 'example',
-          children: [{
-            module: 'app.js'
-          }, {
-            module: 'app.less'
-          }, {
-            module: 'index.html'
-          }]
-        }, {
-          module: 'lib',
-          children: [{
-            module: 'node.js'
-          }, {
-            module: 'react-ui-tree.js'
-          }, {
-            module: 'react-ui-tree.less'
-          }, {
-            module: 'tree.js'
-          }]
-        }, {
-          module: '.gitiignore'
-        }, {
-          module: 'index.js'
-        }, {
-          module: 'LICENSE'
-        }, {
-          module: 'Makefile'
-        }, {
-          module: 'package.json'
-        }, {
-          module: 'README.md'
-        }, {
-          module: 'webpack.config.js'
-        }]
-      }
+      tree: tree
     };
   },
 
