af46d0df2ea3b91015ad5c5fc97239ad9938fef1
Wang Zuo <wzuoadjusted@gmail.com>
2017-07-19 14:49:34 +0800

    update gh-pages 2017-07-19T06:49:34.193Z

--- tree.js
+++ tree.js
@@ -1,68 +1,93 @@
 module.exports = {
   module: 'react-ui-tree',
-  children: [{
-    module: 'dist',
-    collapsed: true,
-    children: [{
-      module: 'node.js',
+  children: [
+    {
+      module: 'dist',
+      collapsed: true,
+      children: [
+        {
+          module: 'node.js',
+          leaf: true
+        },
+        {
+          module: 'react-ui-tree.css',
+          leaf: true
+        },
+        {
+          module: 'react-ui-tree.js',
+          leaf: true
+        },
+        {
+          module: 'tree.js',
+          leaf: true
+        }
+      ]
+    },
+    {
+      module: 'example',
+      children: [
+        {
+          module: 'app.js',
+          leaf: true
+        },
+        {
+          module: 'app.less',
+          leaf: true
+        },
+        {
+          module: 'index.html',
+          leaf: true
+        }
+      ]
+    },
+    {
+      module: 'lib',
+      children: [
+        {
+          module: 'node.js',
+          leaf: true
+        },
+        {
+          module: 'react-ui-tree.js',
+          leaf: true
+        },
+        {
+          module: 'react-ui-tree.less',
+          leaf: true
+        },
+        {
+          module: 'tree.js',
+          leaf: true
+        }
+      ]
+    },
+    {
+      module: '.gitiignore',
       leaf: true
-    }, {
-      module: 'react-ui-tree.css',
+    },
+    {
+      module: 'index.js',
       leaf: true
-    }, {
-      module: 'react-ui-tree.js',
+    },
+    {
+      module: 'LICENSE',
       leaf: true
-    }, {
-      module: 'tree.js',
+    },
+    {
+      module: 'Makefile',
       leaf: true
-    }]
-  }, {
-    module: 'example',
-    children: [{
-      module: 'app.js',
+    },
+    {
+      module: 'package.json',
       leaf: true
-    }, {
-      module: 'app.less',
+    },
+    {
+      module: 'README.md',
       leaf: true
-    }, {
-      module: 'index.html',
+    },
+    {
+      module: 'webpack.config.js',
       leaf: true
-    }]
-  }, {
-    module: 'lib',
-    children: [{
-      module: 'node.js',
-      leaf: true
-    }, {
-      module: 'react-ui-tree.js',
-      leaf: true
-    }, {
-      module: 'react-ui-tree.less',
-      leaf: true
-    }, {
-      module: 'tree.js',
-      leaf: true
-    }]
-  }, {
-    module: '.gitiignore',
-    leaf: true
-  }, {
-    module: 'index.js',
-    leaf: true
-  }, {
-    module: 'LICENSE',
-    leaf: true
-  }, {
-    module: 'Makefile',
-    leaf: true
-  }, {
-    module: 'package.json',
-    leaf: true
-  }, {
-    module: 'README.md',
-    leaf: true
-  }, {
-    module: 'webpack.config.js',
-    leaf: true
-  }]
-}
+    }
+  ]
+};
